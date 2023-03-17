f08ab755d559dd777f105a52bffea89853e2d6e6
Mostafa Mahatabe <mahatabe.omi@gmail.com>
2023-03-17 19:08:36 +0600

    Add project files.

---
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System.Collections.Generic;
+
+namespace BulkyBook.DataAccess;
+
+public class ApplicationDbContext: IdentityDbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Category> Categories { get; set; }
+    public DbSet<CoverType> CoverTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
+	//add-migration extendIdentityUser
+	public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    //add-migration AddCompanyToUser
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+	//add-migration AddOrderHeaderAndDetailToDatabase
+	public DbSet<OrderHeader> OrderHeaders { get; set; }
+	public DbSet<OrderDetail> OrderDetail { get; set; }
+}
